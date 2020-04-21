248873f5a9b8d3499827ca7206863f00420b2af6
Snake <42065561+exxodia666@users.noreply.github.com>
2020-04-22 00:07:08 +0300

    few changes

--- components/WeekForecast.js
+++ components/WeekForecast.js
@@ -1,11 +1,11 @@
 import React from "react";
 import {
-    StyleSheet,
-    Text,
-    View,
-    Button,
-    Image,
-    Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  Dimensions,
 } from "react-native";
 //import { Ionicons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
@@ -20,70 +20,58 @@ import weatherSort from "../utils/weatherSort";
 //const week = [  "Sunday", "Monday", "Tuesday","Wednesday", "Thursday","Friday", "Saturday",];
 
 const WeekForecast = (props) => {
-    const weather = weatherSort(useSelector(state => state.forecast.weather));
-    
-    const week = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Satur"];
+  const weather = weatherSort(useSelector((state) => state.forecast.weather));
 
-    //console.log(weather);
+  const week = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Satur"];
 
+  //console.log(weather);
 
-    const url = (iconName) => `http://openweathermap.org/img/wn/${iconName}@2x.png`;
+  const url = (iconName) =>
+    `http://openweathermap.org/img/wn/${iconName}@2x.png`;
 
-    const createWeekDaysArray = (day) => {
-        const firstDay = day;
-        const data = [];
-        while (day <= 6) {
-            data.push(day);
-            day++;
-        }
-        day = 0;
-        while (day < firstDay) {
-            data.push(day);
-            day++;
-        }
-        return data;
-    };
+  const createWeekDaysArray = (day) => {
+    const firstDay = day;
+    const data = [];
+    while (day <= 6) {
+      data.push(day);
+      day++;
+    }
+    day = 0;
+    while (day < firstDay) {
+      data.push(day);
+      day++;
+    }
+    return data;
+  };
 
-    const labels = createWeekDaysArray(new Date().getDay()).map(
-        (item) => week[item]
-    );
+  const labels = createWeekDaysArray(new Date().getDay()).map(
+    (item) => week[item]
+  );
 
-    return (
-        <View style={styles.container}>
-            {weather.map((item) => {
-                return (
-                    <View style={styles.day} >
-                        <Text>{item.temperature}</Text>
-                        <Image
-                            style={styles.logo}
-                            source={{
-                                uri: url(item.icon),
-                            }}
-                        />
-                    </View>);
-            })}
-
-        </View >
-    );
+  return (
+    <View style={styles.container}>
+      <View style={styles.column}></View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        height: '100%',
-        alignItems: "center",
-        justifyContent: 'center',
-        width: Dimensions.get('window').width,
-        backgroundColor: 'white',
-        elevation: 3,
-        overflow: "hidden",
-        flexDirection: "row"
-    },
-    day: {
-        height: '100%',
-        flex: 1 / 5,
-        borderWidth: 1,
-        borderColor: 'grey'
-    }
+  container: {
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    width: Dimensions.get("window").width,
+    backgroundColor: "white",
+    elevation: 3,
+    overflow: "hidden",
+    flexDirection: "row",
+  },
+  day: {
+    height: "100%",
+    flex: 1 / 5,
+    borderWidth: 1,
+    borderColor: "grey",
+  },
 });
 
 export default WeekForecast;
--- utils/weatherSort.js
+++ utils/weatherSort.js
@@ -1,64 +1,77 @@
-'use strict'
 const weatherSort = (weather) => {
-    const checkArray = []
-    return weather.map((item1) => {
-        if (checkArray.indexOf(item1) === -1) {
-            checkArray.push(item1);
-            return weather.filter((item) => {
-                if (new Date(item1.date).getDay() === new Date(item.date).getDay() && new Date(item1.date).getMonth() === new Date(item.date).getMonth()) {
-                    checkArray.push(item);
-                    return true;
-                }
-            });
-        }
-    }).filter((item) => {
-        return item != undefined;
-    }).map((item) => {
-
-        const morning = [];
-        const day = [];
-        const night = [];
-        item.forEach((item) => {
-            if (new Date(item.date).getHours() <= 11 && new Date(item.date).getHours() > 5) {
-                morning.push(item);
-            }
-            else if (new Date(item.date).getHours() >= 12 && (new Date(item.date).getHours() < 20)) {
-                day.push(item);
-            } else {
-                night.push(item);
-            }
+  console.log(new Date('2020-04-25 21:00:00'));
+  const checkArray = [];
+  return weather
+    .map((item1) => {
+      //console.log(item1);
+      if (checkArray.indexOf(item1) === -1) {
+        checkArray.push(item1);
+       // console.log(typeof(item1.date));
+        return weather.filter((item) => {
+          //console.log(item);
+          if (
+            new Date(item1.date).getDay() === new Date(item.date).getDay() &&
+            new Date(item1.date).getMonth() === new Date(item.date).getMonth()
+          ) {
+            checkArray.push(item);
+            console.log("LOOOOOOOOOOOOOOOOOOOOOOOOOL");
+            return true;
+          }
         });
-        return {
-            morning: morning,
-            day: day,
-            night: night,
+      }
+    })
+    .filter((item) => {
+      return item != undefined;
+    })
+    .map((item) => {
+      const morning = [];
+      const day = [];
+      const night = [];
+      item.forEach((item) => {
+        if (
+          new Date(item.date).getHours() <= 11 &&
+          new Date(item.date).getHours() > 5
+        ) {
+          morning.push(item);
+        } else if (
+          new Date(item.date).getHours() >= 12 &&
+          new Date(item.date).getHours() < 20
+        ) {
+          day.push(item);
+        } else {
+          night.push(item);
         }
-    }).map((item) => {
+      });
+      //console.log(morning[0]);
+      return {
+        morning: morning,
+        day: day,
+        night: night,
+      };
+    })
+    .map((item) => {
+      // console.log(item.night[1]);
+      const weather = {
+        morning: {},
+        day: {},
+        night: {},
+      };
 
-        console.log(item);
-        
-        const weather = {
-            morning: {},
-            day: {},
-            night: {},
-        }
-        
-        for (let key in item) {
-            console.log(key);
-            let temperature = 0;
-            let icon;
-            let date;
-            item[key].forEach((element) => {
-                //console.log(element.icon + element.temperature);
-                temperature = temperature + element.temperature;
-                icon = element.icon;
-                date = element.date;
-            });
-            weather[key].temperature = (temperature / item[key].length).toFixed(2);
-            weather[key].icon = icon;
-            weather[key].date = date;
-        }
-        return { ...weather }
+      for (let key in item) {
+        let temperature = 0;
+        let icon;
+        let date;
+        item[key].forEach((element) => {
+          temperature = temperature + element.temperature;
+          icon = element.icon;
+          date = element.date;
+        });
+        weather[key].temperature = (temperature / item[key].length).toFixed(2);
+        weather[key].icon = icon;
+        weather[key].date = date;
+      }
+
+      return { ...weather };
     });
-}
+};
 export default weatherSort;
